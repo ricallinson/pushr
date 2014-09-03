bd3ad339e543705d458bb1289ccd9a532cd7acc3
Ric Allinson <capecodehq@yahoo.co.uk>
2014-09-03 15:22:50 -0700

    Dir changes

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ A command line utility to push characters to a serial port.
 
 ## Build
 
-	go get github.com/tarm/goserial
-	go build
+	go install pushr
+
+## Run
+
+    ./bin/pushr
---
+++ src/pushr/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "github.com/tarm/goserial"
+    "log"
+    "flag"
+    "strings"
+)
+
+func main() {
+    var port = flag.String("p", "/dev/cu.usbserial-A901LLNP", "the USB port to use")
+    flag.Parse()
+    // Open the serial port.
+    c := &serial.Config{Name: *port, Baud: 9600}
+    s, err := serial.OpenPort(c)
+    if err != nil {
+        log.Fatal(err)
+    }
+    // Send the message.
+    n, err := s.Write([]byte(strings.Join(flag.Args(), " ")))
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("%v", n)
+}
